5f29c63eff3df5d206282ec2b6606cb681f277d1
Bagaskoro Meyca <bagaskoro-int@airyrooms.com>
2019-03-02 17:43:47 +0700

    making Titik class

--- Random.java
+++ Random.java
@@ -5,3 +5,41 @@ public class Random{
      }
      
 }
+
+
+class Titik{
+    private int x,y;
+    
+    public Titik(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * @param x the x to set
+     */
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    /**
+     * @param y the y to set
+     */
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    /**
+     * @return the x
+     */
+    public int getX() {
+        return x;
+    }
+
+    /**
+     * @return the y
+     */
+    public int getY() {
+        return y;
+    }
+}
